20088faa6e4e1ed5a369f75d479135c72ab4c3f5
pabluwu <lopez.pablo2305@gmail.com>
2023-07-08 16:21:48 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import Banner from './components/Banner'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
+
+import Banner from './components/Banner'
+import Footer from './components/Footer'
 
 
 
@@ -19,6 +21,7 @@ function App() {
   return (
     <>
       <Banner/>
+      <Footer/>
     </>
   )
 }
